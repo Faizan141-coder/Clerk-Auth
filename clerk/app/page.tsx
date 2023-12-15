a2ebba2ad8fabc4752121415738d4e869bdd4048
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-12-15 23:40:19 +0500

    added auth route

--- clerk/app/page.tsx
+++ clerk/app/page.tsx
@@ -2,7 +2,7 @@ import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="h-screen p-5">
       <UserButton afterSignOutUrl="/"/>
     </div>
   )
